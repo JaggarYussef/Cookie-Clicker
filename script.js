0312b625a7a8a9e48c8258257779d7f21a8c2e95
JaggarYussef <jaggaryussef@gmail.com>
2022-06-03 11:19:13 +0200

    added functionality of step 3

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+*
+* Cookie Clicker recreation
+*
+* ** Coded by Arnold, Felicien and Jaggar
+*
+*/
+
+
+(() => {
+    let score = 0;
+   document.getElementById("run").addEventListener('click', () => {
+
+    //increase score by one
+     score++
+     console.log(score);
+        
+     
+    })
+
+    
+})();
