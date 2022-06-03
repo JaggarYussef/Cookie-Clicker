2babc51aeab38c11637e63137e33bb6c05ca97a5
JaggarYussef <jaggaryussef@gmail.com>
2022-06-03 14:19:12 +0200

    added step 4 multiplier by 2

--- script.js
+++ script.js
@@ -6,18 +6,51 @@
 *
 */
 
-
+let multiplier= 1;
 let score = 0;
+
+//this function increases $score according to the value of the multiplier. Initially the multiplier is 1 
+//once the multplier button is clicked the value of $multiplier will change accordingly
+function increase(){
+    score += multiplier; 
+
+}
+
+
+
+//click handler 
 document.getElementById("run").addEventListener('click', () => {
 
- //increase score by one
-  score++
-  console.log(score);
+ 
+
+ 
+    increase()
+    console.log(score);
      
   
  })
 
- //______________________________________________BONUS_______________________________
+//Multiplier button handler 
+ document.getElementById("multiplier").addEventListener('click', () => {
+
+ //this makes the click add two the value of score
+  multiplier += 1;
 
 
- //______________________________________________DISPLAY_______________________________
+     
+})
+
+
+/*
+class Multiplier{
+    constructor(name, price, description, perk, clickable){
+        this.name= name;
+        this.price= price;
+        this.description= description;
+        this.clickable= clickable;
+    }
+    
+}
+ */
+
+
