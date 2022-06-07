7abb78b02031879133d9140a0afceb0e090e1fb8
JaggarYussef <jaggarYussef@gmail.com>
2022-06-07 18:45:39 +0200

    autoclicker is added but needs more modification for the increase in cost and value

--- scripts/logic.js
+++ scripts/logic.js
@@ -24,12 +24,47 @@ let instance;
       }
 
 
+   // ********************** STEP 10 OBJECT   ***********************
+      // ______  Auto clicker ______
+      // Once the auto clicker button is clicked the value of $cursor.count becomes is modified 
+      // and increases the score according to give in time in the handler
+
+      let cursor = {
+          value: 2,
+          price: 10,
+          count: 0,
+          description: `The cursor clicks the cookie automatically by ${this.value} `,
+           
+      } 
+
+
+
+      function autoClick( ){
+        if( cursor.count > 0 ){
+
+          let total= cursor.count * cursor.value;
+          autoIncrease(total)
+          cookieCounter.innerHTML = score;  
+
+          console.log("# cursors "+ cursor.count  );
+        }else{
+            console.log("CALLED BUT NOT ENOUGH");
+            console.log("this is score from cursor branch "  +  score );
+            console.log("this is COUNT from cursor branch "  +  cursor.count );
+
+        }
+    }
 
 
 
 
 
 
+
+
+
+
+      
 
 
 
@@ -54,7 +89,13 @@ let multiplierValue= document.getElementById("multiplierInfo");
 //once the multplier button is clicked the value of $multiplier will change accordingly
  function increase(){
     score += multiplier.value; 
+    cookieCounter.innerHTML = score;  
 
+}
+
+function autoIncrease(amount){
+    score += amount;
+    cookieCounter.innerHTML = score;  
 }
 
 
@@ -63,16 +104,27 @@ let multiplierValue= document.getElementById("multiplierInfo");
 document.getElementById("cookie-btn").addEventListener('click', () => {
 
     increase();
-    cookieCounter.innerHTML = score;  
+ 
  })
 
- // ********************* STEP  5 PRICE OF MULTIPLIER ***********************
+
+
+
+
+
+
+
+
+ // ********************* STEP  5 PRICE OF MULTIPLIER         ***********************
+ // ********************* STEP  7 DISPLAY  MULTIPLIER COUNTER ***********************
+ // ********************* STEP  8 PRICE OF MULTIPLIER         ***********************
+ // ********************* STEP 9 MODIFY PRICE OF MULTIPLIER   ***********************
 
 //Multiplier button handler 
  document.getElementById("multiplier").addEventListener('click', () => {
 
- //this makes the click add two the value of score and subtracts $mutliplier.Price from score
- //also displays the value of multiplier **** Multiplication de vos points par : $multiplier.value*****
+//this makes the click add two the value of score and subtracts $mutliplier.Price from score
+//also displays the value of multiplier **** Multiplication de vos points par : $multiplier.value*****
  
 
 
@@ -81,10 +133,15 @@ document.getElementById("cookie-btn").addEventListener('click', () => {
     multiplier.value += 1;
     score -= multiplier.price
     multiplier.price*= (score * 2);
+    
     Math.round(multiplier.price); 
+    Math.round(score); 
+  
     cookieCounter.innerHTML = score;
     multiplierValue.innerHTML= multiplier.value;
 
+  }else{
+    console.log("this is price of " + multiplier.price);
   }
 
 
@@ -92,6 +149,45 @@ document.getElementById("cookie-btn").addEventListener('click', () => {
 
      
 })
+
+
+
+// ********************** STEP 10 AUTOCLICKHANDLER   ***********************
+
+document.getElementById("auto-clicker-1").addEventListener('click', () => {
+
+if( score > cursor.price){
+    console.log("exed");
+    cursor.count += 1;
+    console.log( "cursor count" + cursor.count );
+    score -= cursor.price;
+    cursor.price *= (score * 2);
+    console.log( "price " + cursor.price );
+    setInterval( autoClick, 3000)
+    console.log( "this is score" + score );
+
+}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // class Counter {
